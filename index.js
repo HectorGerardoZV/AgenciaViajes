a8f982869651ad95d9f4669d125b13c721823bb3
TorroSoft <torrozapata@gmail.com>
2021-10-04 21:30:22 -0700

    Cambios

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const port = process.env.PORT || 4000;
 //Habilitar pug
 
 app.set("view engine","pug")
+app.set('views', './views');
 //Obtener el año actual
 app.use((req,res,next)=>{
     const year = new Date();
